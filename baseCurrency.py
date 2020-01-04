6ecb992d6fa0111e400f1863f544ab5473bfcdbd
diwakar <diwakar@fortiate.com>
2020-01-04 13:02:10 +0530

    TV-475 #comment redis argument local or docker

--- baseCurrency.py
+++ baseCurrency.py
@@ -1,6 +1,9 @@
 import redis
-
-redisClient = redis.StrictRedis(host='localhost', port=6379, db=0)
+import hostEnv from loadInMemory
+if hostEnv == 'local':
+    redisClient = redis.StrictRedis('localhost', 6379, db=0)
+elif hostEnv == 'docker':
+    redisClient = redis.StrictRedis('redis', 6379, db=0)
 currency = {}
 def setBaseCurrency():
     baseCurrency = "356"
--- conversionRate.py
+++ conversionRate.py
@@ -1,6 +1,9 @@
 import redis
-
-redisClient = redis.StrictRedis(host='localhost', port=6379, db=0)
+import hostEnv from loadInMemory
+if hostEnv == 'local':
+    redisClient = redis.StrictRedis('localhost', 6379, db=0)
+elif hostEnv == 'docker':
+    redisClient = redis.StrictRedis('redis', 6379, db=0)
 
 converionRate_356 = {"356":1.0000, "840":70.9086, "978":78.5764, "826": 93.3888, "036":48.3025, "784":19.3117, "144":0.390901}
 converionRate_840 = {"356":0.0141027, "840":70.9086, "978":1.12097, "826": 1.32238, "036":0.700104, "784":0.272294, "144":0.00550932}
