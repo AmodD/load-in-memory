945c0265e9ea214c42599d9cd2637b65096dca57
Amod D Kulkarni <amod.d.kulkarni@gmail.com>
2020-01-24 17:24:05 +0530

    TV-542 #comment docker link for chp-dbservice

--- chpListInserter.py
+++ chpListInserter.py
@@ -1,23 +1,28 @@
 import requests
 import json
+import sys
 
-
-def chpList(redisClient):
+def chpList(redisClient,chpdbservice):
     try:
-        response = requests.get("http://localhost:48310/api/chp")
+        response = requests.get(chpdbservice)
         responseText = json.loads(response.text)
         chplist = responseText['payload']['data']
+
         # print(chplist)
         # for i in range(len(chplist)):
         #     print('chp' + str(chplist[i]['ch_CIN']))
         # for i in range(len(chplist)):
         #     print(chplist[i])
+
         for i in range(len(chplist)):
             redisClient.hmset('chp'+str(chplist[i]['ch_CIN']), chplist[i])
             redisClient.rpush('chplist', 'chp'+str(chplist[i]['ch_CIN']))
         print('chp ids loaded into memory successfully')
+#        print('printing response')
+#        print(response)
 
     except Exception as e:
         print(e)
         print('chp-dbservice is not responding')
+        sys.exit(1)
 
--- loadInMemory.py
+++ loadInMemory.py
@@ -3,10 +3,13 @@ import redis
 
 if sys.argv[1] == 'local':
     hostEnv = 'localhost'
+    chpdbservice = 'http://localhost:48310/api/chp'
 elif sys.argv[1] == 'docker':
     hostEnv = 'redis'
+    chpdbservice = 'http://chp-dbservice/api/chp'
 else:
     hostEnv = 'localhost'
+    chpdbservice = 'http://localhost:48310/api/chp'
 
 redisClient = redis.StrictRedis(hostEnv, 6379, db=0)
 
@@ -21,4 +24,4 @@ redisCurrencyCodeDecimalsInserter.redisInserter(redisClient)
 dataElementsSymbols.dataElementInserter(redisClient)
 conversionRate.currrencyConversion(redisClient)
 baseCurrency.setBaseCurrency(redisClient)
-chpListInserter.chpList(redisClient)
+chpListInserter.chpList(redisClient,chpdbservice)
