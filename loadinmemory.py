f963158d56798aa5070ff54b92d0f6a6c3c64024
diwakar <diwakar@fortiate.com>
2020-03-23 17:46:37 +0530

    adding config files

--- loadinmemory.py
+++ loadinmemory.py
@@ -7,15 +7,9 @@ import configparser
 config = configparser.ConfigParser()
 
 env = environ.get('FORTIATE_ENV')
-if env == 'localhost':
-    config.read('../.env.localhost')
-    redis = config['DEFAULT']['REDIS_HOST']
-    chp = config['DEFAULT']['APP_CHP']
-    
-elif env == 'localdocker':
-    config.read('../.env.localdocker')
-    redis = config['DEFAULT']['REDIS_HOST']
-    chp = config['DEFAULT']['APP_CHP']
+config.read('../../CONFIG/'+env)
+redis = config['DEFAULT']['REDIS_HOST']
+chp = config['DEFAULT']['APP_CHP']
 
 alertDomain = 'TM'
 fileName = 'loadinmemory.py'
