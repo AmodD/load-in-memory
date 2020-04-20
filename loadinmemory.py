80a89738d6d992eba5aedcd7053ae0aaa67b4b9b
diwakar <diwakar@fortiate.com>
2020-04-20 10:24:31 +0530

    load in memory restarting fix

--- loadinmemory.py
+++ loadinmemory.py
@@ -10,20 +10,20 @@ try:
     redis_port = os.getenv('REDIS_PORT')
     chp = os.getenv('URL_CHP_DBSERVICE')
 
-    alarm = os.getenv('APP_ALARM')
-    merchants = os.getenv('URL_MERCHANTS_DBSERVICE')
+    # alarm = os.getenv('APP_ALARM')
+    # merchants = os.getenv('URL_MERCHANTS_DBSERVICE')
     if redis_host == None:
         print("redis_host is not se in load-in-memoryt")
         sys.exit(1)
     if chp == None:
         print("chp is not set in load-in-memory")
         sys.exit(1)
-    if alarm == None:
-        print("alarm is not set in load-in-memory")
-        sys.exit(1)
-    if merchants == None:
-        print("merchants is not set in load-in-memory")
-        sys.exit(1)
+    # if alarm == None:
+    #     print("alarm is not set in load-in-memory")
+    #     sys.exit(1)
+    # if merchants == None:
+    #     print("merchants is not set in load-in-memory")
+    #     sys.exit(1)
 except Exception as e:
     print(e)
     sys.exit(1)
@@ -33,17 +33,17 @@ fileName = 'loadinmemory.py'
 
 chpdbservice = chp + 'api/chp'
 redisClient = redis.StrictRedis(redis_host, redis_port, db=0)
-client_url = alarm + 'api/clients'
-try:
-    response = requests.get(client_url)
-    client_json = json.loads(response.text)
-    clients = client_json['payload']['data']
-except Exception as e:
-    fortiatelog(alertDomain, e, '003', 'error', fileName)
-    fortiatelog(alertDomain, 'alarm is not responding', '004', 'error', fileName)
-    sys.exit(1)
+# client_url = alarm + 'api/clients'
+# try:
+#     response = requests.get(client_url)
+#     client_json = json.loads(response.text)
+#     clients = client_json['payload']['data']
+# except Exception as e:
+#     fortiatelog(alertDomain, e, '003', 'error', fileName)
+#     fortiatelog(alertDomain, 'alarm is not responding', '004', 'error', fileName)
+#     sys.exit(1)
 
-fortiatelog(alertDomain, clients, '005', 'info', fileName)
+# fortiatelog(alertDomain, clients, '005', 'info', fileName)
 
 # for client in clients:
 #     mid = merchants+'api/'+client
